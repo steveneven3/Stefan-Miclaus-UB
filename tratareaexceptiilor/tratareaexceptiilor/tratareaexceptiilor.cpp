9b8aa6c5fdd5923b644102223c9db9af6f79c200
steveneven3 <127486187+steveneven3@users.noreply.github.com>
2023-05-22 17:12:28 +0300

    Add files via upload

---
+++ tratareaexceptiilor/tratareaexceptiilor/tratareaexceptiilor.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    unsigned contor = 0;  int div1 = 0;
+    try {
+        for (contor; contor < 5; contor++)
+        {
+            Console::WriteLine("Test test test {0} {1}", "Hello World", contor);
+        }
+        double div = contor / div1;
+        Console::WriteLine(div);
+    }
+    catch (Exception^ ex)
+    {
+        Console::WriteLine("0 exceptie" + ex->Message);
+    }
+            
+            return 0;
+}
